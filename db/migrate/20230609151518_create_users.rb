8fa575143942e8ee6daad0643e157c41e41a47c7
YS <yutaka.7sn@gmail.com>
2023-06-11 10:04:05 +0000

    Finish add-basic-info

--- db/migrate/20230609151518_create_users.rb
+++ db/migrate/20230609151518_create_users.rb
@@ -1,10 +1,5 @@
-class CreateUsers < ActiveRecord::Migration[5.1]
-  def change
-    create_table :users do |t|
-      t.string :name
-      t.string :email
-
-      t.timestamps
+  class AddDepartmentToUsers < ActiveRecord::Migration[5.1]
+    def change
+      add_column :users, :department, :string
     end
   end
-end
